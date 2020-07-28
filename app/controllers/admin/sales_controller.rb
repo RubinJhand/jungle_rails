79c175c89972b416caf60625d3da31cae37c59e4
Rubin <rjhand@gmail.com>
2020-07-28 21:06:29 +0000

    Add model specs for User

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -1,8 +1,39 @@
 class Admin::SalesController < ApplicationController
 
-  # http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"]
-
   def index
-    @sales = Sale.all
+    @sales = Sale.order(starts_on: :asc)
+    @active_sale = Sale.active.first
+  end
+
+  def new
+    @sale = Sale.new
+    @active_sale = Sale.active.first
+  end
+
+  def create
+    @sale = Sale.new(sale_params)
+
+    if @sale.save
+      redirect_to [:admin, :sales], notice: 'Sale created!'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @sale = Sale.find params[:id]
+    @sale.destroy
+    redirect_to [:admin, :sales], notice: 'Sale deleted!'
+  end
+
+  private
+
+  def sale_params
+    params.require(:sale).permit(
+      :name,
+      :percent_off,
+      :starts_on,
+      :ends_on
+    )
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,40 +1,90 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  category = Category.new(name: "Apparel")
-  subject { described_class.new(name: "Jasper's Peppermint Candy", price: 199, quantity: 5, category: category)}
 
+  subject {described_class.new(first_name: 'Jasper', last_name: 'Peppermint', email: 'jpeppermint@fictionalmail.com', password: 'Mmmmm', password_confirmation: 'Mmmmm')}
+  
   describe 'Validations' do
-
-    it "is valid when all four fields save" do
+    
+    it 'saves successfully when all four feilds are set' do
       subject.valid?
-      expect(subject.errors.full_messages).to be_empty
+      expect(subject.errors).to be_empty
+    end
+    
+    it 'fails to save when password is not entered' do
+      subject.password = nil
+      subject.valid?
+      expect(subject.errors).not_to be_empty
     end
 
-    it "is not valid when name is blank" do
-      subject.name = nil
+    it 'fails to save when password confirmation is not entered' do
+      subject.password_confirmation = nil
       subject.valid?
-      expect(subject.errors.full_messages).to include("Name can't be blank")
+      expect(subject.errors).not_to be_empty
     end
 
-    it "is not valid when price is blank" do
-      subject.price_cents = nil
+    it 'fails to save when password and password confirmation do not match' do
+      subject.password_confirmation = 'IHeartTesting'
       subject.valid?
-      expect(subject.errors.full_messages).to include("Price can't be blank")
+      expect(subject.errors).not_to be_empty
     end
 
-    it "is not valid when quantity is blank" do
-      subject.quantity = nil
+    it 'fails to save when email is not entered' do
+      subject.email = nil
       subject.valid?
-      expect(subject.errors.full_messages).to include("Quantity can't be blank")
-
+      expect(subject.errors).not_to be_empty
     end
 
-    it "is not valid when category is blank" do
-      subject.category = nil
+    it 'fails to save when first name is not entered' do
+      subject.first_name = nil
       subject.valid?
-      expect(subject.errors.full_messages).to include("Category can't be blank")
+      expect(subject.errors).not_to be_empty
     end
 
+    it 'fails to save when last name is not entered' do
+      subject.last_name = nil
+      subject.valid?
+      expect(subject.errors).not_to be_empty
+    end
+
+    it 'fails to save when email is not unique regardless if upper or lower case' do
+      User.create(first_name: 'Jasper', last_name: 'Peppermint', email: 'jPeppermint@fictionalmail.com', password: 'Mmmmm', password_confirmation: 'Mmmmm', :password_digest => '111')
+      subject.valid?
+      expect(subject.errors).not_to be_empty
+    end
+
+    it 'fails to save when password and password_confirmation is less than length 5' do
+      subject.password = '1111'
+      subject.password_confirmation = '1111'
+      subject.valid?
+      expect(subject.errors).not_to be_empty
+    end
+
+    describe '.authenticate_with_credentials' do
+      it 'returns user if succesfully authenticated' do
+        subject.save
+        user = User.authenticate_with_credentials('jpeppermint@fictionalmail.com', 'Mmmmm')
+        expect(subject).to be == user
+      end
+
+      it 'returns nil if not successfully authenticated' do
+        subject.save
+        user = User.authenticate_with_credentials('jpeppermint@fictionalmail.com', '999')
+        expect(user).to be == nil
+      end
+
+      it 'authenticates and users if user type white space before / after email' do
+        subject.save
+        user = User.authenticate_with_credentials(' jpeppermint@fictionalmail.com', 'Mmmmm')
+        expect(subject).to be == user
+      end
+
+      it 'authenticates and users if users type lower and upper case in email' do
+        subject.save
+        user = User.authenticate_with_credentials('jpeppermint@fictionalmail.com', 'Mmmmm')
+        expect(subject).to be == user
+      end
+    end
+    
   end
 end
