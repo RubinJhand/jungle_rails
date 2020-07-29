3fe3a1be7491833ed445a3bcf29a5bb02e2639d6
Rubin <rjhand@gmail.com>
2020-07-29 00:43:11 +0000

    Add Product Details feature spec

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor click one of the product partials and get to new page", type: :feature, js: true do
+
+    # SETUP
+    before :each do
+      @category = Category.create! name: 'Apparel'
+
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+
+  scenario "They see all products" do
+
+    # ACT
+    visit root_path
+
+    # VERIFY
+    page.first('article.product').find('h4').click
+    find('a.btn-default').click
+    expect(page).to have_css 'article.product'
+    expect(page).to have_content('Price')
+
+    # DEBUG
+    save_screenshot 'product_details.png'
+
+  end
+end 
